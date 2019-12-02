7a5d0964997ead7707b18e5960cc7c89f83f41c6
TroboticsCode <TroboticsCode@gmail.com>
2019-12-02 16:19:51 -0500

    starting driveFunctions files

---
+++ include/DriveFunctionsConfig.h
@@ -0,0 +1,11 @@
+#ifndef DRIVEFUNCTIONSCONFIG_H
+#define DRIVEFUNCTIONSCONFIG_H
+
+//this is where all the config variables for a robot are located. Change them to match your robot
+//these variables are used in DriveFunctions.cpp
+
+/* chassis config */
+  //uncomment the 
+#define C
+
+#endif
